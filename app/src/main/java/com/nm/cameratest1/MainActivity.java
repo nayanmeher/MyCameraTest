a09e31767b526951b1e3ea2879a57458c14602cd
nayanmeher <nayanmeher16@gmail.com>
2019-07-15 23:40:11 +0530

    CameraDevice added

--- app/src/main/java/com/nm/cameratest1/MainActivity.java
+++ app/src/main/java/com/nm/cameratest1/MainActivity.java
@@ -1,11 +1,55 @@
 package com.nm.cameratest1;
 
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextureView mTextureView;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener= new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            closeCamera();
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            closeCamera();
+        }
+    };
+
 
 
 
@@ -13,6 +57,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextureView = findViewById(R.id.textureView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mTextureView.isAvailable()){
+
+        }
+        else {
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
     }
 
     @Override
@@ -27,8 +85,15 @@ public class MainActivity extends AppCompatActivity {
                     View.SYSTEM_UI_FLAG_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-
         }
 
+    }
+
+
+    private void closeCamera(){
+        if(mCameraDevice != null){
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
     }
 }
